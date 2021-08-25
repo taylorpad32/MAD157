df94d7fddee93401170d1fb4632cc306bf930992
taylorpad32 <69467320+taylorpad32@users.noreply.github.com>
2021-08-25 17:00:05 -0500

    Add files via upload

---
+++ HelloWorldPaulT/HelloWorldPaulT/HelloWorldPaulTApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldPaulTApp.swift
+//  HelloWorldPaulT
+//
+//  Created by paul taylor on 8/25/21.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldPaulTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
