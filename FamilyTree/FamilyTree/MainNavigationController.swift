e36d803e48e5a2233784a8f10f26c7236a5b921f
taylorpad32 <69467320+taylorpad32@users.noreply.github.com>
2021-09-16 13:46:13 -0500

    Add files via upload

---
+++ FamilyTree/FamilyTree/MainNavigationController.swift
@@ -0,0 +1,17 @@
+//
+//  MainNavigationController.swift
+//  FamilyTree
+//
+//  Created by paul taylor on 9/15/21.
+//
+
+import Foundation
+import UIKit
+
+class MainNavigationController: UINavigationController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ FamilyTree/FamilyTree/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FamilyTree
+//
+//  Created by paul taylor on 9/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
