7ea52c4d6ec269b72b2af04efca42561963f32ca
taylorpad32 <69467320+taylorpad32@users.noreply.github.com>
2021-09-19 13:53:30 -0500

    Add files via upload

---
+++ FamilyTreeTwo/FamilyTree/MainNavigationController.swift
@@ -0,0 +1,31 @@
+//
+//  MainNavigationController.swift
+//  FamilyTree
+//
+//  Created by paul taylor on 9/15/21.
+//
+
+import Foundation
+import UIKit
+
+class MainNavigationController: UINavigationController { //UINavigationController
+  //  var pictures = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+   //     let fm = FileManager.default
+   //     let path = Bundle.main.resourcePath!
+    //    let items = try! fm.contentsOfDirectory(atPath: path)
+        
+    //    for item in items {
+     //       if item.hasPrefix("city") {
+       //         pictures.append(item)
+        //    }
+      //  }
+     //   print(pictures)
+    }
+        
+    }
+    
+
---
+++ FamilyTreeTwo/FamilyTree/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MySecondApp
+//
+//  Created by paul taylor on 9/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {     //UIViewController
+
+   
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+}
+
+
+
+
+
+
