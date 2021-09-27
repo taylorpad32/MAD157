ea8233c9c1968cd30ea654f00182de63e18f9348
taylorpad32 <69467320+taylorpad32@users.noreply.github.com>
2021-09-26 22:11:14 -0500

    Add files via upload

---
+++ tabbedMiniApp copyNew0924/tabbedMiniApp/CardioNeuroController.swift
@@ -0,0 +1,21 @@
+//
+//  CardiologyController.swift
+//  tabbedMiniApp
+//
+//  Created by paul taylor on 9/22/21.
+//
+
+//import Foundation
+
+import UIKit
+
+class CardioNeuroController: UIViewController {
+
+    @IBOutlet var labelResult: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        labelResult.numberOfLines = 0
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ tabbedMiniApp copyNew0924/tabbedMiniApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tabbedMiniApp
+//
+//  Created by paul taylor on 9/22/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var btnImage: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+  
+}
