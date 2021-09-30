9a1d19880ecc51d3cd637a9f92ec9876492bf29a
taylorpad32 <69467320+taylorpad32@users.noreply.github.com>
2021-09-30 09:31:31 -0500

    Add files via upload

---
+++ changeBackgroundImage/changeBackgroundImage/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  changeBackgroundImage
+//
+//  Created by paul taylor on 9/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    let colors = [UIColor.red, UIColor.blue, UIColor.green, UIColor.purple]
+    
+    var index = 0
+    
+    @IBAction func ButtonAction(_ sender: Any) {
+        
+        self.view.backgroundColor = colors[index]
+        
+        if index == colors.count - 1 {
+            index = 0
+    }
+        else {
+            index += 1
+        }
+    
+    }
+
+}
+
